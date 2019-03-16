e68c1e7e5e80e7a96a8146ab280931029ba5def6
EJ Chathuranga Ziphio <ejChathuranga@users.noreply.github.com>
2019-03-16 06:35:48 +0530

    auth response complete

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// class for authenticate user
+// 
+
 class Auth extends CI_Controller {
 
 	function __construct(){
@@ -8,15 +11,9 @@ class Auth extends CI_Controller {
 		$this->load->helper('url');
 	}
 
-	public function index()
-	{
-		$query = $this->db->query('SELECT * FROM `roll`');
-		echo json_encode($query->result());
-	}
-
+	// user auth endpoint 
 	public function login()
 	{
-		//echo "string";
 
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
@@ -25,10 +22,6 @@ class Auth extends CI_Controller {
 
 		if ($this->auth_model->can_login($username, $password)) {
 
-			// $response->data = $this->auth_model->can_login($username, $password);
-			// $response->code = 1;
-			// $response->message = "Authenticate success"
-
 			$response = array(
 				'data' => $this->auth_model->can_login($username, $password),
 				'code' => 1,
@@ -36,20 +29,14 @@ class Auth extends CI_Controller {
 				);
 
 			echo json_encode($response);
-			
-			// $session_data = array('username' => $username);
-
-			// $this->session->set_userdata($session_data);
-			// redirect(base_url().'auth/enter');
 
 		}else{
-
-			$response->code = 2;
-			$response->message = "Authenticate fails";
-			$response->data = "";
-			echo $response;
-			// $this->session->set_flashdata('error', 'invalid username and password');
-			// redirect(base_url().'auth/login');
+			$response = array(
+				'data' => null,
+				'code' => 2,
+				'message' => "Authenticate fails"
+			);
+			echo json_encode($response);
 		}
 	}
 
--- application/models/auth_model.php
+++ application/models/auth_model.php
@@ -1,5 +1,6 @@
 <?php
 
+// model for auth user request. Inhere will check existing user's and weill genarate response according to that
 class Auth_model extends CI_Model{
 
 	function can_login($un, $pw){
@@ -9,7 +10,31 @@ class Auth_model extends CI_Model{
 
 		$query = $this->db->get('employee');
 		if ($query->num_rows()>0) {
-			return $query->result();
+			
+
+			$id = $query->result()[0]->id;
+			$name = $query->result()[0]->name;
+			$username = $query->result()[0]->username;
+			$address = $query->result()[0]->address;
+			$bod = $query->result()[0]->bod;
+			$gender = $query->result()[0]->gender;
+			$roll_id = $query->result()[0]->roll_id;
+			$branch_id = $query->result()[0]->branch_id;
+
+			$userModel = array(
+				'id' => $id,
+				'name' => $name,
+				'username' => $username,
+				'address' => $address,
+				'bod' => $bod,
+				'gender' => $gender,
+				'roll_id' => $roll_id,
+				'branch_id' => $branch_id,
+				);
+
+			return $userModel;
+
+			//return $query->result();
 			//return true;
 		}else{
 			return NULL;
